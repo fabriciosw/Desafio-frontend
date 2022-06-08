2c70b40467e622220d3ecaff915eae36b183ce73
Fabricio <fabricio.seb1@gmail.com>
2022-06-08 17:17:37 -0300

    refactor: table componentized
    Change-Id: I93e59b9d6a6f7ca1bcaa8f0c95690c9b0321b2bf

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Table, TableCell, TableHead, TableRow } from '@mui/material';
+import IUser from '../../interfaces/IUser';
+import DeleteUserButton from '../DeleteUserButtonModal';
+import EditUserButton from '../EditUserButtonModal';
+import { AuthenticationContext } from '../../contexts/AuthenticationContext';
+import formatDateBR from '../../utils/formatDateBR';
+
+interface Props {
+  users: IUser[];
+  setUsers: React.Dispatch<React.SetStateAction<IUser[]>>;
+}
+export default function UsersTable({ users, setUsers }: Props): JSX.Element {
+  const { isAdmin } = AuthenticationContext();
+
+  return (
+    <Table sx={{ minWidth: 650 }} size="small" aria-label="Tabela de usuários">
+      <TableHead>
+        <TableRow>
+          <TableCell align="center">Nome</TableCell>
+          <TableCell align="center">Data de nascimento</TableCell>
+          <TableCell align="center">CPF</TableCell>
+          <TableCell align="center">Permissão</TableCell>
+          <TableCell align="center">Observação</TableCell>
+          {isAdmin ? <TableCell align="center">Gerenciar</TableCell> : ''}
+        </TableRow>
+      </TableHead>
+
+      {users.map((user) => (
+        <TableRow hover key={user.cpf}>
+          <TableCell align="center">{user.name}</TableCell>
+          <TableCell align="center">{formatDateBR(user.birthDate)}</TableCell>
+          <TableCell align="center">{user.cpf}</TableCell>
+          <TableCell align="center">{user.permission ? 'Administrador' : 'Colaborador'}</TableCell>
+          <TableCell align="center" sx={{ maxWidth: 400 }}>
+            {user.obs}
+          </TableCell>
+          {isAdmin ? (
+            <TableCell align="center">
+              <DeleteUserButton id={user.id} setUsers={setUsers} />
+              <EditUserButton user={user} setUsers={setUsers} />
+            </TableCell>
+          ) : (
+            ''
+          )}
+        </TableRow>
+      ))}
+    </Table>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Table, TableCell, TableHead, TableRow } from '@mui/material';
 import IUser from '../../interfaces/IUser';
 import { logoutUser } from '../../services/session.service';
 import { getUsers } from '../../services/users.service';
-import CreateUserButton from '../../components/CreateUserButton';
-import DeleteUserButton from '../../components/DeleteUserButton';
-import EditUserButton from '../../components/EditUserButton';
+import CreateUserButton from '../../components/CreateUserButtonModal';
 import toastMsg, { ToastType } from '../../utils/toastMsg';
 import { AuthenticationContext } from '../../contexts/AuthenticationContext';
-import formatDateBR from '../../utils/formatDateBR';
+import UsersTable from '../../components/Table';
 
 export default function Home(): JSX.Element {
   const { isAdmin } = AuthenticationContext();
@@ -34,39 +31,8 @@ export default function Home(): JSX.Element {
       </button>
 
       {isAdmin ? <CreateUserButton setUsers={setUsers} /> : ''}
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Nome</TableCell>
-            <TableCell align="center">Data de nascimento</TableCell>
-            <TableCell align="center">CPF</TableCell>
-            <TableCell align="center">Permissão</TableCell>
-            <TableCell align="center">Observação</TableCell>
-            {isAdmin ? <TableCell align="center">Gerenciar</TableCell> : ''}
-          </TableRow>
-        </TableHead>
-        {users.map((user) => (
-          <>
-            <TableRow hover key={user.cpf}>
-              <TableCell align="center">{user.name}</TableCell>
-              <TableCell align="center">{formatDateBR(user.birthDate)}</TableCell>
-              <TableCell align="center">{user.cpf}</TableCell>
-              <TableCell align="center">{user.permission ? 'Administrador' : 'Colaborador'}</TableCell>
-              <TableCell align="center" sx={{ maxWidth: 400 }}>
-                {user.obs}
-              </TableCell>
-              {isAdmin ? (
-                <TableCell align="center">
-                  <DeleteUserButton id={user.id} setUsers={setUsers} />
-                  <EditUserButton user={user} setUsers={setUsers} />
-                </TableCell>
-              ) : (
-                ''
-              )}
-            </TableRow>
-          </>
-        ))}
-      </Table>
+
+      <UsersTable users={users} setUsers={setUsers} />
     </>
   );
 }
